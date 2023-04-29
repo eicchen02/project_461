7f6cdd2bda57870cc5d20cf526aa9580f291bfe1
Matthew-Nale <mnale@DESKTOP-76VD33J.localdomain>
2023-04-28 22:54:58 -0400

    Changed comments to follow specific style

--- app.py
+++ app.py
@@ -11,52 +11,64 @@ id_counter = 0
 application = connexion.FlaskApp(__name__, specification_dir='Rest_API_File/')
 app = application.app
 
-#! Implement RESTful API here, but first update the app.routes to be in accordance with SWAGGER
+#* Frontend Page navigation
 
-#Frontend Page navigation
+#? This is the homepage for the website
 @app.route('/')
 def home():
     return render_template('Home.html')
 
+#? This is the page which lists all database commands.
 @app.route('/database/')
 def database():
     return render_template('Database.html')
 
+#? This is the list of packages.
+#! May choose to not do this. Currently have no way of displaying all packages, so may want to ignore this
 @app.route('/database/packages/')
 def packageList():
     return render_template('PackageList.html')
 
+#? This is the upload page
+#TODO Must use backend services to complete upload. May be able to use calls further below to accomplish this
 @app.route('/database/upload')
 def upload():
     return render_template('interactions/Upload.html')
 
+#? This is the update page
+#TODO Must use backend services to complete update. May be able to use calls further below to accomplish this
 @app.route('/database/update/')
 def update():
     return render_template('/interactions/Update.html')
 
+#? This is the rate page
+#TODO Must use backend services to complete rate. May be able to use calls further below to accomplish this
 @app.route('/database/rate/')
 def rate():
     return render_template('interactions/Rate.html')
 
+#? This is the download page
+#TODO Must use backend services to complete download. May be able to use calls further below to accomplish this
 @app.route('/database/download/')
 def download():
     return render_template('/interactions/Download.html')
 
-# Backend paths and operations
+#* Backend paths and operations
 
 #? We have no authentication, should return 501
+#TODO May have to include tokens anyways? Maybe not users, but potentially tokens for operations. May not be baseline though.
 @app.route('/authenticate', methods=["PUT"])
 def CreateAuthToken():
     return jsonify({'status_code': '501',
                     'message': 'This system does not support authentication'}), 501, {'content_type': 'application/json'}
 
-#? /package path uploads a new package
+#? Uploads a new package to our database
 @app.route('/package', methods=["POST"])
 def PackageCreate():
-    # Grabs the global id_counter for adding packages
+    #* Grabs the global id_counter for adding packages
     global id_counter
     
-    # Loads and checks the data from the request
+    #* Loads and checks the data from the request
     data = json.loads(request.data)
     if "URL" in data:
         file = data["URL"]
@@ -66,16 +78,16 @@ def PackageCreate():
         return jsonify({'status_code': '400',
                         'message': 'There is missing field(s) in the PackageData or it is formed inproperly'}), 400, {'content_type': 'application/json'}
     
-    # Open the temp_file for writing the data to
+    #* Open the temp_file for writing the data to
     f = open("temp_file.txt", "w+")
     f.write(file)
     f.close()
 
-    # Runs the upload function from ./run, which calculates scoring
+    #* Runs the upload function from ./run, which calculates scoring
     subprocess.run(["./run", "upload", "temp_file.txt"])
     sleep(2)
     
-    # Grabs the URL and scores from the output json
+    #* Grabs the URL and scores from the output json
     output = load(open("output/output.json"))[0]
     obtainedURL = output["URL"]
     netScore = output["NET_SCORE"]
@@ -87,7 +99,7 @@ def PackageCreate():
     responsiveness = output["RESPONSIVE_MAINTAINER_SCORE"]
     license = output["LICENSE_SCORE"]
     
-    # Performs Ingestion on the package
+    #* Performs Ingestion on the package
     if (netScore < 0.5 or rampup < 0.5 or updatedCode < 0.5
         or pinningPractice < 0.5 or correctness < 0.5
         or busFactor < 0.5 or responsiveness < 0.5
@@ -95,142 +107,157 @@ def PackageCreate():
         return jsonify({'status_code': '424',
                         'message': 'Package is not uploaded due to the disqualified rating.'}), 424, {'content_type': 'application/json'}
     
-    # Remove unneeded files
+    #* Remove unneeded files
     remove("output/output.json")
     remove("temp_file.txt")
 
     #! Still needs to check if already in database and return error code 409 if it already exists
     #! Still needs to upload to database after previous checks
     
-    # Grab version number from package
+    #* Grab version number from package
     version = load(open(f'local_cloning/cloned_repos/{os.path.basename(obtainedURL)}/package.json'))["version"]
 
-    # Form metadata return json
+    #* Form metadata return json
     metadata = {'ID': f'{id_counter}',
                 'Name': f'{os.path.basename(obtainedURL)}',
                 'Version': f'{version}'}
     
-    # Increment id_counter
+    #* Increment id_counter
     id_counter += 1
     
-    # Form data return json
+    #* Form data return json
     packageData = {'URL': f'{obtainedURL}'}
 
-    # Return success
+    #* Return success
     return jsonify({'status_code': '201',
                     'message': 'Success. Check the ID in the returned metadata for the official ID.',
                     'data': f'{packageData}',
                     'metadata': f'{metadata}'}), 201, {'content_type': 'application/json'}
 
+#? This will delete packages from SQL database by the name provided
+#! Needs SQL interaction
 @app.route('/package/byName/{name}', methods=["DELETE"])
 def PackageByNameDelete(name=None):
-    # Check if a name is actually passed, and return error if not
+    #* Check if a name is actually passed, and return error if not
     if name == None:
         return jsonify({'status_code': '400',
                         'message': 'Package name cannot be \'None\'.'}), 400, {'content_type': 'application/json'}
     
-    # First, check if package name is in our SQL database. Return 404 if not in database
+    #* First, check if package name is in our SQL database. Return 404 if not in database
     #! Need to implement searching by name for SQL database
 
-    # Then, use SQL commands to delete package
+    #* Then, use SQL commands to delete package
     #! Need to implement SQL connection to delete package
     
-    # Finally, return 200 if everything is successful
+    #* Finally, return 200 if everything is successful
     return jsonify({'status_code': '200',
                     'message': 'Package is deleted'}), 200, {'content_type': 'application/json'}
 
+#? This will obtain the most recent change to the package (date of last modification)
+#! Needs SQL interaction, as well as additional table entry for 'time last modified'
 @app.route('/package/byName/{name}', methods=["GET"])
 def PackageByNameGet(name=None):
-    # Check if a name is actually passed, and return error if not
+    #* Check if a name is actually passed, and return error if not
     if name == None:
         return jsonify({'status_code': '400',
                         'message': 'Package name cannot be \'None\'.'}), 400, {'content_type': 'application/json'}
     
-    # First, check if the package name is in our SQL database. Return 404 if not in database
+    #* First, check if the package name is in our SQL database. Return 404 if not in database
     #! Need to implement searching by name for SQL database
     
-    # Then, use SQL commands to obtain when the package was entered into the SQL database
+    #* Then, use SQL commands to obtain when the package was entered into the SQL database
     #! Need to implement SQL functionality, as well as potentially uploading time of upload into table
     
-    # Finally, return 200 with correct message
+    #* Finally, return 200 with correct message
     #! Need to implement SQL functionality to return correct format. Almost correct format is described here:
     ''' return jsonify({'status_code': '200',
                         'message' : 'Obtained the history of the given package. Check in the \'history\' field for the data.',
                         'history' : f'{HISTORY_DATE (Example: 2023-03-23T23:11:15.000Z)}'}), 200, {'content_type': 'application/json'}
     '''
     
-    # Temp return for now
+    #* Temp return for now
     return jsonify({'status_code': '200',
                     'message': 'This is a test, must implement later'}), 200, {'content_type': 'application/json'}
 
-#? This uses the search function that we have created
+#? This uses the search function that we have created to search for a list of Package types
+#! Needs SQL interaction
 @app.route('/package/byRegEx', methods=["POST"])
 def PackageByRegExGet():
-    # Loads and checks the data from the request.
-    #! Assuming currently that this is saved in the json under the 'regex' key. May not be in actuality
+    #* Loads and checks the data from the request.
+    #TODO Assuming currently that this is saved in the json under the 'regex' key. May not be in actuality
     regex = json.loads(request.data)["regex"]
     if regex == None:
         return jsonify({'status_code': '400',
                         'message': 'There must be a regular expression that can be used in the body.'}), 400, {'content_type': 'application/json'}
     
-    # Otherwise, perform search function through SQL database
+    #* Otherwise, perform search function through SQL database
     #! Need to implement SQL functionality to search correctly for a RegEx expression through READMEs and names
     
-    # If the package is not found, return 404 error
+    #* If the package is not found, return 404 error
     #! Need to implement SQL functionality before checking.
     
-    # Otherwise, return a list of all packages that match
+    #* Otherwise, return a list of all packages that match
     #! Need to implement SQL functionality beforehand. Returns should be of type PackageMetadata, which includes ID, packageName, and version
     
-    # Temp return for now
+    #* Temp return for now
     return jsonify({'status_code': '200',
                     'message': 'This is a test, must implement later'}), 200, {'content_type': 'application/json'}
 
+#? This deletes packages based on the id provided
+#! Needs SQL interaction
 @app.route('/package/{id}', methods=["DELETE"])
 def PackageDelete(id=None):
-    # First, check if ID is none
+    #* First, check if ID is none
     if id == None:
         return jsonify({'status_code': '400', 
                         'message': 'There must be an ID value in order to delete packages'}), 400, {'content_type': 'application/json'}
     
-    # Then, check if package is within SQL database by ID
+    #* Then, check if package is within SQL database by ID
     #! Need to implement SQL functionality beforehand to search through database. Return 404 if not in database
     
-    # Finally, return 200 if deleted
+    #* Then, delete package from SQL database
+    #! Need to implement SQL functionality beforehand to interact with database.
+    
+    #* Finally, return 200 if deleted
     return jsonify({'status_code': '200',
                     'message': 'Package is deleted'}), 200, {'content_type': 'application/json'}
 
+#? This retrieves a base64 package from the database
+#! Needs SQL interaction
 @app.route('/package/{id}', methods=["GET"])
 def PackageRetrieve(id=None):
-    # First, check if ID is none
+    #* First, check if ID is none
     if id == None:
         return jsonify({'status_code': '400',
                         'message': 'There must be an ID value in order to delete packages'}), 400, {'content_type': 'application/json'}
     
-    # Then, check if package exists in the SQL database
+    #* Then, check if package exists in the SQL database
     #! Need to implement SQL functionality beforehand to search through database. Return 404 if not in database
     
-    # Then, convert package into base64/obtain base64 package for PackageData type field
+    #* Then, convert package into base64/obtain base64 package for PackageData type field
     #! Need to implement SQL functionality beforehand to obtain the URL/Base64 package uploaded.
     
-    # Finally, return Package to user. Should follow the 'Package' Schema, which involves 'PackageData' and 'PackageMetadata'
+    #* Finally, return Package to user. Should follow the 'Package' Schema, which involves 'PackageData' and 'PackageMetadata'
     #! Need to implement SQL first to get data
+    
+    #* Temp return for now
     return jsonify({'code': '200',
                     'message': 'This is a test, must implement later'}), 200, {'content_type': 'application/json'}
 
 #? This will replace the current ID with a new package version (update), as long as ID, version, and name match
+#! Needs SQL interaction
 @app.route('/package/{id}', methods=["PUT"])
 def PackageUpdate(id=None):
-    # First, check if ID is none
+    #* First, check if ID is none
     if id == None:
         return jsonify({'status_code': '400',
                         'message': 'There must be an ID value in order to delete packages'}), 400, {'content_type': 'application/json'}
     
-    # Next, obtain data from response.
-    #! Assuming that the data is found in 'data'. Might be wrong and need to change
+    #* Next, obtain data from response.
+    #TODO Assuming that the data is found in 'data'. Might be wrong and need to change
     data = json.loads(request.data)
     
-    # Then, check that there is an ID, name, and version provided
+    #* Then, check that there is an ID, name, and version provided
     if not data["id"]:
         return jsonify({'status_code': '400',
                         'message': 'There are missing field(s) in the request body. There must be an \'id\', \'name\', and \'version\'.'}), 400, {'content_type': 'application/json'}
@@ -241,32 +268,33 @@ def PackageUpdate(id=None):
         return jsonify({'status_code': '400',
                         'message': 'There are missing field(s) in the request body. There must be an \'id\', \'name\', and \'version\'.'}), 400, {'content_type': 'application/json'}
     
-    # Then, check if id/name/version match in database. Start with id, then check name and version associated with that id
+    #* Then, check if id/name/version match in database. Start with id, then check name and version associated with that id
     #! Need to implement SQL functionality beforehand to search through database. Return error code 404 if it does not match
 
-    # Update the values for the id
+    #* Update the values for the id
     #! Need to implement SQL functionality beforehand. Then, do the same as for PackageCreate does.
     
-    # Finally, return 200 status code
+    #* Finally, return 200 status code
 
     return jsonify({'status_code': '200',
                     'message': 'Version is updated'}), 200, {'content_type': 'application/json'}
 
 #? This is the rating function for a package's id value
+#! Needs SQL interaction
 @app.route('/package/{id}/rate', methods=["GET"])
 def PackageRate(id=None):
-    # First, check if id is None
+    #* First, check if id is None
     if id == None:
         return jsonify({'status_code': '400',
                         'message': 'There must be a valid ID in order to rate packages'}), 400, {'content_type': 'application/json'}
     
-    # Then, obtain the scores from the SQL database
+    #* Then, obtain the scores from the SQL database
     #! Need to implement SQL functionality to obtain store values. If the package ID does not exist, return 404. If a score is -1 or failed, return 500.
     
-    # Then, need to format scores according to the PackageRating definition in the YAML.
+    #* Then, need to format scores according to the PackageRating definition in the YAML.
     #! Need SQL scores to do this
     
-    # Finally, return 200 and PackageRating if successful
+    #* Finally, return 200 and PackageRating if successful
     return jsonify({'status_code': '200',
                     'message': 'Rating was successful. View the scores for the package in the \'rate\' section.',
                     'rate': 'This is a test, put values here when completed'}), 200, {'content_type': 'application/json'}
@@ -286,9 +314,8 @@ def RegistryReset():
                     'message': 'Registry is reset'}), 200, {'content_type': 'application/json'}
 
 
-# Main function call to start backend/frontend
+#* Main function call to start backend/frontend
 if __name__ == "__main__":
-    # app.run(host='localhost', port=8080)
     application.add_api('api-spec.yaml')
     app.static_folder = ('templates/static')
     app.template_folder = ('templates')
