c10887b95db86a1812138ab0e112ca040fcf4e13
Matthew-Nale <mnale@DESKTOP-76VD33J.localdomain>
2023-05-01 23:19:34 -0400

    Various additions to the app.py program

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify, send_file
+import connexion
 import json
 import subprocess
 import os
-import connexion
+from input.toEncodedPackage import createEncodedFile
 from json import load
 from time import sleep
 from os import remove
-from input.toEncodedPackage import createEncodedFile
 from sys import stderr
 from sql.sql_header import *
 
@@ -44,6 +44,7 @@ def packageList():
 def upload():
     # If the submit button is pressed (A "POST" Operation)
     if (request.method == "POST"):
+        
         # Sets the default status to be an error
         session['upload_status'] = "Error with Uploading"
         
@@ -59,7 +60,6 @@ def upload():
         # Check if the current URL already is uploaded
         # Return error if already in database
         exist = exists(table.c.PackageLink, package_url)
-
         if exist is True:
             session['upload_status_details'] = "Package is already uploaded, try updating instead!"
             return redirect(url_for('uploadComplete'))
@@ -76,7 +76,7 @@ def upload():
         # Grabs the URL and scores from the output json
         output = load(open("output/output.json"))[0]
 
-        #! Performs Ingestion on the package (disabled)
+        # Performs Ingestion on the package (enabled)
         for score in output.values():
             if score < 0.5:
                 session['upload_status_details'] = "Package is not uploaded due to a disqualified rating.\nEvery metric must score greater than 0.5"
@@ -104,6 +104,7 @@ def upload():
 def update():
     # If the submit button is pressed (A "POST" Operation)
     if (request.method == "POST"):
+        
         # Sets default status message to be an error
         session['update_status'] = "Error with Updating"
         
@@ -156,6 +157,7 @@ def update():
 def rate():
     # If the submit button is pressed (A "POST" Operation)
     if (request.method == "POST"):
+        
         # Sets the default status to be an error
         session['rate_status'] = "Error with Rating"
         
@@ -173,13 +175,10 @@ def rate():
         columns =  ["PackageName", "PackageLink", "Responsiveness", "UpdatedCode", "Correctness", "BusFactor", "RampUp", "Licensing", "Pinning", "NetScore"]
         query = db.select(table).where(table.c.PackageLink == package_url)
         result = connection.execute(query).fetchall()
-        print(result, file = stderr)
-        #! needs to actually display on the frontend
 
         # Return rating page that shows all scores according to the data gathered
-        #TODO Change the session 'status_details' to be the values obtained from SQL database
         session['rate_status'] = "Package has been Rated"
-        session['rate_status_details'] = "Here are the values for the given package!"
+        session['rate_status_details'] = result
         return redirect(url_for('rateComplete'))
     else:
         # Otherwise, just return the Rate page (A "GET" Operation)
@@ -204,16 +203,9 @@ def download():
             session['download_status'] = "No such package exists!"
             return redirect(url_for('downloadComplete'))
         
-        # Grab the base64 package associated from the SQL database
-        #! Need SQL interaction. Return error page if failed
-        
-        # Use this format to send the file back -> return send_file(io.BytesIO(FILE_DATA))
-        #! Need SQL interaction. Return error page if failed
-        
-        # Show success page on success
-        session['download_status'] = "Package has been Downloaded"
-        session['download_status_details'] = "The given package has been downloaded from our database!"
-        return redirect(url_for('downloadComplete'))
+        # Create the Base64 package, and return the package
+        encodedPackage = createEncodedFile(package_url)
+        return send_file(encodedPackage, as_attachment=True)
     else:
         # Otherwise, just return the Download page (A "GET" Operation)
         return render_template('/interactions/Download.html')
@@ -262,7 +254,6 @@ def downloadComplete():
 # user.
 
 #? We have no authentication, should return 501
-#TODO May have to include tokens anyways? Maybe not users, but potentially tokens for operations. May not be baseline though.
 @app.route('/authenticate', methods=["PUT"])
 def CreateAuthToken():
     return jsonify({'status_code': '501',
@@ -272,6 +263,7 @@ def CreateAuthToken():
 #? the "Upload" Frontend Page Operation.
 @app.route('/package', methods=["POST"])
 def PackageCreate():   
+    
     # Loads and checks the data from the request
     data = json.loads(request.data)
     if "URL" in data:
@@ -295,13 +287,13 @@ def PackageCreate():
     output = load(open("output/output.json"))[0]
     obtainedURL = output["URL"]
 
+    # Checks if the url is already in the SQL database
     exist = exists(table.c.PackageLink, obtainedURL)
-
     if exist is True:
        return jsonify({'status_code': '409',
                         'message': 'Package exists already.'}), 409, {'content_type': 'application/json'}
             
-    #! Performs Ingestion on the package (disabled)
+    # Performs Ingestion on the package (enabled)
     for score in output.values():
         if score < 0.5:
             session['upload_status_details'] = "Package is not uploaded due to a disqualified rating.\nEvery metric must score greater than 0.5"
@@ -310,27 +302,31 @@ def PackageCreate():
     # Upload to SQL database
     subprocess.run(["python3", "sql/upload.py"])
 
-    # Then, remove unneeded files
+    # Then, remove unneeded files from local directories
     remove("output/output.json")
     remove("temp_link.txt")
 
-    # Still needs to check if already in database and return error code 409 if it already exists (DO THIS HERE) DONE
-    # Still needs to upload to database after previous checks (DO THIS HERE) DONE
-    
-    # Grab version number from package
-    version = load(open(f'local_cloning/cloned_repos/{os.path.basename(obtainedURL)}/package.json'))["version"]
+    # Grab version number from package.json. Returns error 404 if it does not exist
+    try:
+        version = load(open(f'local_cloning/cloned_repos/{os.path.basename(obtainedURL)}/package.json'))["version"]
+    except:
+        return jsonify({'status_code': '404',
+                        'message': 'The provided package does not have a valid \'package.json\''}), 404, {'content_type': 'application/json'}
 
-    #Get ID
+    #Get ID from SQL database
     query = db.select(table.c.ID).where(table.c.PackageLink == obtainedURL)
     return_id = connection.execute(query).scalar()
-    # Form metadata return json
+    
+    # Form the metadata return field
     metadata = {'ID': f'{return_id}',
                 'Name': f'{os.path.basename(obtainedURL)}',
                 'Version': f'{version}'}
     
-    # Form data return json
-    #TODO Should return base64 package, instead of URL
-    packageData = {'URL': f'{obtainedURL}'}
+    # Form the data return field. Contains the Base64 package and URL
+    base64PackageName = createEncodedFile(obtainedURL)
+    with open(base64PackageName, 'rb') as encodedPackage:
+        packageData = {'Content': f'{encodedPackage.read()}',
+                       'URL': f'{obtainedURL}'}
 
     # Return success
     return jsonify({'status_code': '201',
@@ -348,20 +344,15 @@ def PackageByNameDelete(name=None):
                         'message': 'Package name cannot be \'None\'.'}), 400, {'content_type': 'application/json'}
     
     # First, check if package name is in our SQL database. Return 404 if not in database
-    #Need to implement searching by name for SQL database (done)
     exist = exists(table.c.PackageName, name)
-
     if exist is False:
        return jsonify({'status_code': '404',
                         'message': 'Package doesn\'t exist.'}), 404, {'content_type': 'application/json'}
-            
 
     # Then, use SQL commands to delete package
-    # Need to implement SQL connection to delete package 
     query = db.delete(table).where(table.c.PackageName == name)
     connection.execute(query)
     connection.commit()
-    
     
     # Finally, return 200 if everything is successful
     return jsonify({'status_code': '200',
@@ -378,11 +369,9 @@ def PackageByNameGet(name=None):
     
     # First, check if the package name is in our SQL database. Return 404 if not in database
     exist = exists(table.c.PackageName, name)
-
     if exist is False:
        return jsonify({'status_code': '404',
                         'message': 'Package doesn\'t exist.'}), 404, {'content_type': 'application/json'}
-    
     
     # Then, use SQL commands to obtain when the package was entered into the SQL database
     query = db.select(table.c.LastModified).where(table.c.PackageName == name)
@@ -397,6 +386,7 @@ def PackageByNameGet(name=None):
 #? for a list of Package types.
 @app.route('/package/byRegEx', methods=["POST"])
 def PackageByRegExGet():
+    
     # Loads and checks the data from the request.
     #TODO Assuming currently that this is saved in the json under the 'regex' key. May not be in actuality
     regex = json.loads(request.data)["regex"]
@@ -405,21 +395,30 @@ def PackageByRegExGet():
                         'message': 'There must be a regular expression that can be used in the body.'}), 400, {'content_type': 'application/json'}
     
     # Otherwise, perform search function through SQL database
+    packageMatches = []
     #! Need to implement SQL functionality to search correctly for a RegEx expression through READMEs and names
     
     # If the package is not found, return 404 error
-    #! Need to implement SQL functionality before checking.
+    if packageMatches:
+        return jsonify({'status_code': '404',
+                        'message': 'No package found under this regex'}), 400, {'content_type': 'application/json'}
+    
+    packageMetadata = []
+    #TODO Psuedocode Listed Here:
+    #TODO for package in packageMatches:
+    #TODO     packageMetadata.append({'ID': f'{package["ID"]}',
+    #TODO                             'Name': f'{package["Name"]}',
+    #TODO                             'Version': f'{package["Version"]}})
     
     # Otherwise, return a list of all packages that match
-    #! Need to implement SQL functionality beforehand. Returns should be of type PackageMetadata, which includes ID, packageName, and version
-    
-    # Temp return for now
     return jsonify({'status_code': '200',
-                    'message': 'This is a test, must implement later'}), 200, {'content_type': 'application/json'}
+                    'message': 'Attached is the list of packages. Check the \'data\' field for the list',
+                    'data': f'{packageMatches}'}), 200, {'content_type': 'application/json'}
 
 #? This deletes packages based on the id provided.
 @app.route('/package/{id}', methods=["DELETE"])
 def PackageDelete(id=None):
+    
     # First, check if ID is none
     if id == None:
         return jsonify({'status_code': '400', 
@@ -427,7 +426,6 @@ def PackageDelete(id=None):
     
     # Then, check if package is within SQL database by ID
     exist = exists(table.c.ID, id)
-
     if exist is False:
        return jsonify({'status_code': '404',
                         'message': 'Package doesn\'t exist.'}), 404, {'content_type': 'application/json'}
@@ -443,16 +441,19 @@ def PackageDelete(id=None):
 
 #? This retrieves a base64 package from the database. This 
 #? operates similarly to the 'Download" Frontend Page Operation.
-#! Needs SQL interaction
 @app.route('/package/{id}', methods=["GET"])
 def PackageRetrieve(id=None):
+    
     # First, check if ID is none
     if id == None:
         return jsonify({'status_code': '400',
                         'message': 'There must be an ID value in order to delete packages'}), 400, {'content_type': 'application/json'}
     
     # Then, check if package exists in the SQL database
-    #! Need to implement SQL functionality beforehand to search through database. Return 404 if not in database
+    exist = exists(table.c.ID, id)
+    if exist is False:
+       return jsonify({'status_code': '404',
+                        'message': 'Package doesn\'t exist.'}), 404, {'content_type': 'application/json'}
     
     # Then, convert package into base64/obtain base64 package for PackageData type field
     #! Need to implement SQL functionality beforehand to obtain the URL/Base64 package uploaded.
@@ -469,13 +470,13 @@ def PackageRetrieve(id=None):
 #? aka update by ID
 @app.route('/package/{id}', methods=["PUT"])
 def PackageUpdate(id=None):
+    
     # First, check if ID is none
     if id == None:
         return jsonify({'status_code': '400',
                         'message': 'There must be an ID value in order to delete packages'}), 400, {'content_type': 'application/json'}
     
     # Next, obtain data from response.
-    #TODO Assuming that the data is found in 'data'. Might be wrong and need to change
     data = json.loads(request.data)
     
     # Then, check that there is an ID, name, and version provided
@@ -516,15 +517,14 @@ def PackageUpdate(id=None):
 #? operates similarly to the "Rate" Frontend Page Operation.
 @app.route('/package/{id}/rate', methods=["GET"])
 def PackageRate(id=None):
+    
     # First, check if id is None
     if id == None:
         return jsonify({'status_code': '400',
                         'message': 'There must be a valid ID in order to rate packages'}), 400, {'content_type': 'application/json'}
     
-    
     # Then, check if package is within SQL database by ID
     exist = exists(table.c.ID, id)
-
     if exist is False:
        return jsonify({'status_code': '404',
                         'message': 'Package doesn\'t exist.'}), 404, {'content_type': 'application/json'}
