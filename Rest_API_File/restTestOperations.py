3f8ac7d9dc5eacded2a290634bce35739c6b6234
Eric Chen <eicchen02@gmail.com>
2023-05-03 03:54:52 -0400

    Added modifier for local or gcp based running

--- Rest_API_File/restTestOperations.py
+++ Rest_API_File/restTestOperations.py
@@ -1,45 +1,48 @@
 import requests
 import base64
 import zipfile
+from sys import argv
 
-def main():
+
+
+def main(link):
+
     package = 'cloudinary_npm'
-    id = '12'
     
     #* Setup of header for all tests
     header = {'X-Authorization': 'Test'}
     
     #* Testing (Authenticate)
-    response = requests.put("https://project-461-xm2e3izt6a-uc.a.run.app/authenticate", json={'Secret': {'password': 'TestTest'},
+    response = requests.put( link + "/authenticate", json={'Secret': {'password': 'TestTest'},
                                                                         'User': {'isAdmin': True,
                                                                                  'name': 'Test'}})
     print(f'Authentication Test: {response.json()}\n')
     
     # #* Testing (Package Create)
-    # response = requests.post("https://project-461-xm2e3izt6a-uc.a.run.app/package", headers=header, json={'Content': '',
-    #                                                                                 'JSProgram': '', 
-    #                                                                                 'URL': 'https://github.com/cloudinary/cloudinary_npm'})
-    # print(f'Package Create Test, Grabbing JSON: {response.json()}')
-    # if(response.status_code == 201):
-    #     data = response.json()['data']
-    #     print(f'Package Create Test, Grabbing data: {data}')
-    #     metadata = response.json()['metadata']
-    #     print(f'Package Create Test, Grabbing metadata: {metadata}\n')
-    #     package = metadata["Name"]
-    #     id = metadata["ID"]
-    #     version = metadata["Vers"]
-    # else:
-    #     print(f'The package was not uploaded, due to an error code {response.status_code}\n')
+    response = requests.post(link + "/package", headers=header, json={'Content': '',
+                                                                                    'JSProgram': '', 
+                                                                                    'URL': 'https://github.com/cloudinary/cloudinary_npm'})
+    print(f'Package Create Test, Grabbing JSON: {response.json()}')
+    if(response.status_code == 201):
+        data = response.json()['data']
+        print(f'Package Create Test, Grabbing data: {data}')
+        metadata = response.json()['metadata']
+        print(f'Package Create Test, Grabbing metadata: {metadata}\n')
+        package = metadata["Name"]
+        id = metadata["ID"]
+        version = metadata["Vers"]
+    else:
+        print(f'The package was not uploaded, due to an error code {response.status_code}\n')
     
     #TODO Testing (Package By Name History)
-    response = requests.get("https://project-461-xm2e3izt6a-uc.a.run.app/package/byName/" + package, headers=header)
+    response = requests.get(link + "/package/byName/" + package, headers=header)
     if(response.status_code == 200):
         print(f'Package By Name History Test: {response.json()}\n')
     else:
         print(f'The package history could not be obtained by name, due to an error code {response.status_code}\n')
     
     # #TODO Testing (Package By RegEx Search)
-    # response = requests.get("https://project-461-xm2e3izt6a-uc.a.run.app/package/byRegEx", headers=header, json={'regex': 'testRegEx'})
+    # response = requests.get(link + "/package/byRegEx", headers=header, json={'regex': 'testRegEx'})
     # if(response.status_code == 200):
     #     print(f'Package By RegEx Search Test: {response.json()}\n')
     # else:
@@ -47,7 +50,7 @@ def main():
     
     
     #TODO Testing (Package By ID Retrieve)
-    response = requests.get("https://project-461-xm2e3izt6a-uc.a.run.app/package/" + id, headers=header)
+    response = requests.get(link + "/package/" + id, headers=header)
     if(response.status_code == 200):
         if response.json()["data"]["Content"] != None:
             print("Package By ID Retrieve Test: Correctly content field\n")
@@ -57,21 +60,21 @@ def main():
         print(f'The package could not be retrieved by ID, due to an error code {response.status_code}\n')
     
     # #TODO Testing (Package By ID Update)
-    # response = requests.get("https://project-461-xm2e3izt6a-uc.a.run.app/package/" + id, headers=header)
+    # response = requests.get(link + "/package/" + id, headers=header)
     # if(response.status_code == 200):
     #     print(f'Package By ID Update Test: {response.json()}\n')
     # else:
     #     print(f'The package could not be retrieved by ID, due to an error code {response.status_code}\n')
 
     #TODO Testing (Package By ID Rate)
-    response = requests.get("https://project-461-xm2e3izt6a-uc.a.run.app/package/" + id + "/rate", headers=header)
+    response = requests.get(link + "/package/" + id + "/rate", headers=header)
     if(response.status_code == 200):
         print(f'Package By ID Rate Test: {response.json()}\n')
     else:
         print(f'The package could not be rated by ID, due to an error code {response.status_code}\n')
     
     #TODO Testing (Packages Fetch)
-    response = requests.post("https://project-461-xm2e3izt6a-uc.a.run.app/packages", headers=header)
+    response = requests.post(link + "/packages", headers=header)
     if(response.status_code == 200):
         print(f'Packages Fetch Test: {response.json()}\n')
     else:
@@ -80,7 +83,7 @@ def main():
     #*DELETE ZONE (too spicy, please only use one)
 
     # #TODO Testing (Package By Name Delete)
-    # response = requests.delete("https://project-461-xm2e3izt6a-uc.a.run.app/package/byName/" + package, headers=header)
+    # response = requests.delete(link + "/package/byName/" + package, headers=header)
     # if(response.status_code == 200):
     #     print(f'Package By Name Deletion Test: {response.json()}\n')
     # else:
@@ -88,7 +91,7 @@ def main():
 
 
     #TODO Testing (Package By ID Delete)
-    response = requests.delete("https://project-461-xm2e3izt6a-uc.a.run.app/package/0", headers=header)
+    response = requests.delete(link + "/package/0", headers=header)
     if(response.status_code == 200):
         print(f'Package By ID Delete Test: {response.json()}\n')
     else:
@@ -96,11 +99,17 @@ def main():
     
     
     # #TODO Testing (Database Reset)
-    # response = requests.delete("https://project-461-xm2e3izt6a-uc.a.run.app/reset", headers=header)
+    # response = requests.delete(link + "/reset", headers=header)
     # if(response.status_code == 200):
     #     print(f'Database Reset Test: {response.json()}\n')
     # else:
     #     print(f'The database could not be reset, due to an error code {response.status_code}\n')
     
 if __name__ == "__main__":
-    main()
+    # print(argv)
+    if(argv[1] == "-l"):
+        link = "http://127.0.0.1:8080"
+    else: 
+        link = "https://project-461-xm2e3izt6a-uc.a.run.app"
+
+    main(link)
