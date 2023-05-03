6119e255043347d68695678e722437104ce80029
Eric Chen <eicchen02@gmail.com>
2023-05-03 17:40:12 -0400

    f

--- sql/sql_header.py
+++ sql/sql_header.py
@@ -7,53 +7,53 @@ import os
 #
 #? Works with Whitelisted IP (local)
 
-def connect_tcp_socket() -> db.engine.base.Engine:
-    ip_type = IPTypes.PRIVATE if os.environ.get("PRIVATE_IP") else IPTypes.PUBLIC
+# def connect_tcp_socket() -> db.engine.base.Engine:
+#     ip_type = IPTypes.PRIVATE if os.environ.get("PRIVATE_IP") else IPTypes.PUBLIC
 
-    connector = Connector(ip_type)
+#     connector = Connector(ip_type)
 
-    # function to return the database connection
-    def getconn() -> pymysql.connections.Connection:
-        conn: pymysql.connections.Connection = connector.connect(
-            os.environ.get("SQL_LINK", "No SQL_LINK"),
-            "pymysql",
-            user=os.environ.get("SQL_USER", "No SQL_USER"),
-            password=os.environ.get("SQL_PASSWORD", "No SQL_Password"),
-            db=os.environ.get("SQL_DB", "No SQL_DB")
-        )
-        return conn
+#     # function to return the database connection
+#     def getconn() -> pymysql.connections.Connection:
+#         conn: pymysql.connections.Connection = connector.connect(
+#             os.environ.get("SQL_LINK", "No SQL_LINK"),
+#             "pymysql",
+#             user=os.environ.get("SQL_USER", "No SQL_USER"),
+#             password=os.environ.get("SQL_PASSWORD", "No SQL_Password"),
+#             db=os.environ.get("SQL_DB", "No SQL_DB")
+#         )
+#         return conn
 
-    # create connection pool
-    pool = db.create_engine(
-        "mysql+pymysql://",
-        creator=getconn,
-        # echo=True
-    )
-    return pool
+#     # create connection pool
+#     pool = db.create_engine(
+#         "mysql+pymysql://",
+#         creator=getconn,
+#         # echo=True
+#     )
+#     return pool
 
 #? Works with Cloud Run
 
-# def connect_tcp_socket() -> db.engine.base.Engine:
-#     db_host = '10.65.176.3'  # e.g. '127.0.0.1' ('172.17.0.1' if deployed to GAE Flex)
-#     db_user = os.environ["SQL_USER"]  # e.g. 'my-db-user'
-#     db_pass = os.environ["SQL_PASSWORD"]  # e.g. 'my-db-password'
-#     db_name = os.environ["SQL_DB"]  # e.g. 'my-database'
-#     db_port = 3306  # e.g. 3306
+def connect_tcp_socket() -> db.engine.base.Engine:
+    db_host = '10.65.176.3'  # e.g. '127.0.0.1' ('172.17.0.1' if deployed to GAE Flex)
+    db_user = os.environ["SQL_USER"]  # e.g. 'my-db-user'
+    db_pass = os.environ["SQL_PASSWORD"]  # e.g. 'my-db-password'
+    db_name = os.environ["SQL_DB"]  # e.g. 'my-database'
+    db_port = 3306  # e.g. 3306
 
-#     pool = db.create_engine(
-#         # Equivalent URL:
-#         # mysql+pymysql://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>
-#         db.engine.url.URL.create(
-#             drivername="mysql+pymysql",
-#             username=db_user,
-#             password=db_pass,
-#             host=db_host,
-#             port=db_port,
-#             database=db_name,
-#         ),
-#         # ...
-#     )
-#     return pool
+    pool = db.create_engine(
+        # Equivalent URL:
+        # mysql+pymysql://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>
+        db.engine.url.URL.create(
+            drivername="mysql+pymysql",
+            username=db_user,
+            password=db_pass,
+            host=db_host,
+            port=db_port,
+            database=db_name,
+        ),
+        # ...
+    )
+    return pool
 
 pool = connect_tcp_socket()
 connection = pool.connect()
